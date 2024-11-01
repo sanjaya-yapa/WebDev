a6eaac3f3530bae1bbc2ae28a874b4363c791b3a
sanjaya-yapa <sanjaya_yapa@hotmail.com>
2024-11-02 09:20:44 +1100

    Add all the web files in the practive folder

---
+++ JQUERY/base/js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  $(".red-box").fadeOut(2000);
+  $(".green-box").fadeOut(5000);
+  $(".red-box").fadeIn(1000);
+});
